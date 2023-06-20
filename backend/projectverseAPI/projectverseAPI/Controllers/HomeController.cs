34572411bae5e39784cd0d2bf26cfa37694715f8
oiskra <ojaskra@gmail.com>
2023-06-20 15:34:58 +0200

    init

---
+++ backend/projectverseAPI/projectverseAPI/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace projectverseAPI.Controllers
+{
+    [ApiController]
+    [Route("home")]
+    public class HomeController : Controller
+    {
+        [HttpGet(Name = "GetTest")]
+        public IActionResult Index()
+        {
+            return Ok("Test");
+        }
+    }
+}
