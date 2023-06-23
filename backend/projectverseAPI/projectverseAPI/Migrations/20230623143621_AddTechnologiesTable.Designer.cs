abcafbc5524ab2c938836dd44c04f4246c076138
oiskra <ojaskra@gmail.com>
2023-06-23 17:46:10 +0200

    created dbseeder

--- backend/projectverseAPI/projectverseAPI/Migrations/20230623143621_AddTechnologiesTable.Designer.cs
+++ backend/projectverseAPI/projectverseAPI/Migrations/20230623143621_AddTechnologiesTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using projectverseAPI.Data;
 
@@ -11,9 +12,11 @@ using projectverseAPI.Data;
 namespace projectverseAPI.Migrations
 {
     [DbContext(typeof(ProjectVerseContext))]
-    partial class ProjectVerseContextModelSnapshot : ModelSnapshot
+    [Migration("20230623143621_AddTechnologiesTable")]
+    partial class AddTechnologiesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/projectverseAPI/projectverseAPI/Migrations/20230623143621_AddTechnologiesTable.cs
@@ -0,0 +1,108 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace projectverseAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTechnologiesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Technology_Collaborations_CollaborationId",
+                table: "Technology");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Technology_Projects_ProjectId",
+                table: "Technology");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Technology",
+                table: "Technology");
+
+            migrationBuilder.RenameTable(
+                name: "Technology",
+                newName: "Technologies");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Technology_ProjectId",
+                table: "Technologies",
+                newName: "IX_Technologies_ProjectId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Technology_CollaborationId",
+                table: "Technologies",
+                newName: "IX_Technologies_CollaborationId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Technologies",
+                table: "Technologies",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Technologies_Collaborations_CollaborationId",
+                table: "Technologies",
+                column: "CollaborationId",
+                principalTable: "Collaborations",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Technologies_Projects_ProjectId",
+                table: "Technologies",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Technologies_Collaborations_CollaborationId",
+                table: "Technologies");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Technologies_Projects_ProjectId",
+                table: "Technologies");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Technologies",
+                table: "Technologies");
+
+            migrationBuilder.RenameTable(
+                name: "Technologies",
+                newName: "Technology");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Technologies_ProjectId",
+                table: "Technology",
+                newName: "IX_Technology_ProjectId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Technologies_CollaborationId",
+                table: "Technology",
+                newName: "IX_Technology_CollaborationId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Technology",
+                table: "Technology",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Technology_Collaborations_CollaborationId",
+                table: "Technology",
+                column: "CollaborationId",
+                principalTable: "Collaborations",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Technology_Projects_ProjectId",
+                table: "Technology",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id");
+        }
+    }
+}
