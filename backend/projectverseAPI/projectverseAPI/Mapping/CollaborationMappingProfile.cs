0f568e6fd0f639f51f92fed8b47d4f01e57e44ec
oiskra <ojaskra@gmail.com>
2023-07-06 16:45:48 +0200

    created mapping profile for dto and models

---
+++ backend/projectverseAPI/projectverseAPI/Mapping/CollaborationMappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using projectverseAPI.DTOs.Collaboration;
+using projectverseAPI.Interfaces;
+using projectverseAPI.Models;
+
+namespace projectverseAPI.Mapping
+{
+    public class CollaborationMappingProfile : AutoMapper.Profile
+    {
+        public CollaborationMappingProfile()
+        {
+            //CreateCollab
+            CreateMap<CreateCollaborationDTO, Collaboration>()
+                .ForMember(
+                    x => x.Id,
+                    opt => opt.MapFrom(src => Guid.NewGuid()));
+            CreateMap<CreateCollaborationPositionDTO, CollaborationPosition>()
+                .ForMember(
+                    x => x.Id,
+                    opt => opt.MapFrom(src => Guid.NewGuid()));
+
+            //Update
+            CreateMap<UpdateCollaborationDTO, Collaboration>();
+
+            //GetCollab
+            CreateMap<Collaboration, CollaborationDTO>();
+            CreateMap<User, CollaborationAuthorDTO>();
+
+        }
+    }
+}
