661545eb7ed8008bd16e5f0926aab5b5e6ce763a
oiskra <ojaskra@gmail.com>
2023-07-12 14:19:52 +0200

    registration

---
+++ backend/projectverseAPI/projectverseAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using projectverseAPI.DTOs.Authentication;
+using projectverseAPI.Interfaces;
+using projectverseAPI.Migrations;
+using projectverseAPI.Models;
+using System.Security.Cryptography;
+
+namespace projectverseAPI.Controllers
+{
+    [ApiController]
+    [Route("api/auth")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IAuthenticationService _authenticationService;
+
+        public AuthenticationController(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        [AllowAnonymous]
+        public async Task<ActionResult<IdentityResult>> Register(UserRegisterDTO request)
+        {
+            var user = new User
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = request.UserName,
+                Email = request.Email,                
+            };
+
+            var response = await _authenticationService.RegisterUser(user, request.Password);
+
+            if (!response.Succeeded) return BadRequest(response);
+
+            return CreatedAtAction("register", response);
+        }
+
+
+    }
+}
---
+++ backend/projectverseAPI/projectverseAPI/Services/AuthenticationService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using projectverseAPI.Interfaces;
+using projectverseAPI.Models;
+
+namespace projectverseAPI.Services
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AuthenticationService(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+            _roleManager.
+        }
+
+        public async Task<bool> RegisterUser(User user, string password)
+        {
+            var userExists = _userManager.FindByEmailAsync(user.Email);
+
+            if (userExists is not null) return false;
+
+            var result = await _userManager.CreateAsync(user, password);
+
+            return result.Succeeded;
+        }
+    }
+}
