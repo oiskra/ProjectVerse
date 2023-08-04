bf0043972ffe59925689b0de6f89fb674e58f495
oiskra <ojaskra@gmail.com>
2023-08-04 16:55:40 +0200

    token response on login

--- backend/projectverseAPI/projectverseAPI/Controllers/AuthenticationController.cs
+++ backend/projectverseAPI/projectverseAPI/Controllers/AuthenticationController.cs
@@ -68,7 +68,7 @@ namespace projectverseAPI.Controllers
                         Status = StatusCodes.Status401Unauthorized,
                         Errors = null
                     }) 
-                : Ok(result);
+                : Ok(new TokenResponseDTO { Token = result });
         }
 
         [HttpPost]
