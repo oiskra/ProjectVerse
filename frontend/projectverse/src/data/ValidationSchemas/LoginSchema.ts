a64397cc3b00c02990e586ce6f9b612c41ff36bd
oiskra <80036800+oiskra@users.noreply.github.com>
2024-04-17 13:13:54 +0200

    Delete frontend directory

--- frontend/projectverse/src/data/ValidationSchemas/LoginSchema.ts
+++
@@ -1,8 +0,0 @@
-import * as Yup from 'yup'
-
-const LoginSchema = Yup.object({
-  email:Yup.string().required(),
-  password:Yup.string().required()
-})
-
-export default LoginSchema;
--- frontend/projectverse/src/pages/PageLoading.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const PageLoading = () => {
-  return (
-    <div>PageLoading</div>
-  )
-}
